70bbfe8c5ce434eff9954e65c8c79f096ae7efa5
Wouter Voorbrood <wbvoorbrood@gmail.com>
2023-10-18 14:46:08 +0200

    restructure index js and add helper functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import './style.css'
-import Jas from './assets/jas.jpg'
+import './style.css';
+import Jas from './assets/jas.jpg';
 import Bier from './assets/moersleutel.png';
 import Bb_shirt from './assets/bb_shirt.jpg';
 import Shirt from './assets/shirt.jpg';
@@ -12,7 +12,7 @@ document.body.appendChild(mainContainer);
 
 // create image container
 const sliderContainer = document.createElement('div');
-sliderContainer.classList.add('slider')
+sliderContainer.classList.add('slider');
 mainContainer.appendChild(sliderContainer);
 
 // create div for buttons
@@ -23,67 +23,71 @@ document.body.appendChild(buttonDiv);
 // Create slider array to hold images
 const imageArray = [];
 
+// add counter to count position in images array
+let imageArrayCounter = 0;
+
+// Add images to image slider array
 addSliderImage(Jas, imageArray);
 addSliderImage(Bier, imageArray);
 addSliderImage(Bb_shirt, imageArray);
 addSliderImage(Shirt, imageArray);
 addSliderImage(Cameo, imageArray);
 
-function addSliderImage(importedImage, sliderImageArray){
-    // Adds images to slider array
-    const image = new Image();
-    image.src = importedImage;
-    sliderImageArray.push(importedImage);
+// add interface buttons
+addNextButton(buttonDiv);
+addPreviousButton(buttonDiv);
+
+function addNextButton(container) {
+	// create "next"-button to control the images
+	const nextBtn = document.createElement('button');
+	nextBtn.innerText = 'Next Image';
+	buttonDiv.appendChild(nextBtn);
+
+	nextBtn.onclick = () => {
+		showImage();
+
+		//increment the imageArrayCounter
+		imageArrayCounter += 1;
+
+		if (imageArrayCounter === imageArray.length) {
+			imageArrayCounter = 0;
+		}
+	};
 }
 
-// add counter to count position in images array
-let imageArrayCounter = 0;
+function addPreviousButton(container) {
+	// create "previous"-button to control the images
+	const prevBtn = document.createElement('button');
+	prevBtn.innerText = 'Previous Image';
+	buttonDiv.appendChild(prevBtn);
 
-// create "next"-button to control the images
-const nextBtn = document.createElement('button');
-nextBtn.innerText = 'Next Image';
-buttonDiv.appendChild(nextBtn);
+	prevBtn.onclick = () => {
+		imageArrayCounter -= 1;
+		if (imageArrayCounter < 0) {
+			imageArrayCounter = imageArray.length - 1;
+		}
 
-nextBtn.onclick = () => {
-    
-    showImage();
-
-    //increment the imageArrayCounter
-    imageArrayCounter += 1;
-
-    if (imageArrayCounter === imageArray.length) {
-        imageArrayCounter = 0;
-    }
-
+		showImage();
+	};
 }
 
-// create "previous"-button to control the images
-const prevBtn = document.createElement('button');
-prevBtn.innerText = 'Previous Image';
-buttonDiv.appendChild(prevBtn);
+// Helper Functions
 
-prevBtn.onclick = () => {
-    imageArrayCounter -= 1;
-    if(imageArrayCounter < 0) {
-        imageArrayCounter = imageArray.length-1
-    }
-
-    // New function from HERE
-    
-    showImage();
-
-    // TO HERE
-
+function addSliderImage(importedImage, sliderImageArray) {
+	// Adds images to slider array
+	const image = new Image();
+	image.src = importedImage;
+	sliderImageArray.push(importedImage);
 }
 
 function showImage() {
-    if(sliderContainer.childNodes.length > 0){
-        sliderContainer.removeChild(sliderContainer.lastChild);
-    }
+	if (sliderContainer.childNodes.length > 0) {
+		sliderContainer.removeChild(sliderContainer.lastChild);
+	}
 
-    const image = new Image();
-    image.src = imageArray[imageArrayCounter];
-    sliderContainer.appendChild(image)
+	const image = new Image();
+	image.src = imageArray[imageArrayCounter];
+	sliderContainer.appendChild(image);
 
-    console.log(imageArrayCounter)
+	console.log(imageArrayCounter);
 }
